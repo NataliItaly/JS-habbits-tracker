9811786c3e06e6772bc0817915cbe6cace5928a6
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2025-06-01 00:59:29 +0200

    feat: delete comment

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,11 @@
 'use strict';
 import data from '../data/demo.json'  with { type: 'json' };
 
-console.log(data);
+
 
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY';
+let activeID = 0;
 
 const page = {
   menu: document.querySelector('.menu__list'),
@@ -12,22 +13,30 @@ const page = {
     h1: document.querySelector('.h1'),
     progressPercent: document.querySelector('.progress__percent'),
     progressCoverBar: document.querySelector('.progress__cover-bar')
-
+  },
+  main: {
+    main: document.querySelector('main'),
+    days: document.querySelector('.days'),
+    habbit: {
+      day: document.querySelector('.habbit__day'),
+      form: document.querySelector('.habbit__form'),
+      delete: document.querySelector('.habbit__delete')
+    }
   }
 }
 window.addEventListener('DOMContentLoaded',()=>{
-  console.log("DOMContentLoaded");
+
     if(!localStorage.getItem(HABBIT_KEY)) {
       saveData(data);
     }
-
     loadData()
-    rerender(habbits[0].id)
+    //console.log(habbits)
+    rerender(habbits[activeID].id)
   });
 
 function loadData() {
   const habbitsString = localStorage.getItem(HABBIT_KEY);
-  console.log(habbitsString)
+
   const habbitArr = JSON.parse(habbitsString);
 
   if (Array.isArray(habbitArr)) {
@@ -36,13 +45,14 @@ function loadData() {
 }
 
 function saveData(value) {
+  console.log('value from save', value)
   localStorage.setItem(HABBIT_KEY, JSON.stringify(value));
 }
 
 //(() => loadData())();
 
-function rerenderMenu(activeHabbit) {
-  if (!activeHabbit) return;
+function renderMenu(activeHabbit) {
+  page.main.main.innerHTML = '';
 
   for (const habbit of habbits) {
     const element = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
@@ -65,20 +75,88 @@ function rerenderMenu(activeHabbit) {
       element.classList.remove('menu__item_active');
     }
   }
+
+  activeID = activeHabbit.id
 }
 
 function renderHead(activeHabbit) {
   if (!activeHabbit) return;
   page.header.h1.innerText = activeHabbit.name
   const progress = activeHabbit.days.length / activeHabbit.target > 1 ? 100 : activeHabbit.days.length / activeHabbit.target * 100;
-  page.header.progressPercent.innerHTML = progress.toFixed(0) + '%'
+  page.header.progressPercent.innerHTML = `${progress.toFixed(0)}%`
   page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`)
 }
 
-function rerender(activeHabbitId) {
-  const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId)
-  rerenderMenu(activeHabbit)
+function renderBody(activeHabbit) {
+  activeHabbit.days.forEach((day, i) => {
+    const currentHabbit = document.createElement('div');
+    currentHabbit.classList.add('habbit');
+    const currentDay = document.createElement('div');
+    currentDay.classList.add('habbit__day');
+    currentDay.textContent = `Day ${i + 1}`;
+    currentHabbit.append(currentDay);
+
+    if (day.comment !== "") {
+      const comment = (`
+          <div class="habbit__comment">
+            <p class="habbit__comment-text">${day.comment}</p>
+            <button class="button habbit__delete">Delete</button>
+          </div>
+        `);
+      currentHabbit.insertAdjacentHTML('beforeend', comment);
+    }
+    else {
+      const form = (`<form class="habbit__form">
+        <input
+        name="comment"
+        class="input_icon"
+        type="text"
+        placeholder="Comment"
+        />
+        <img
+        class="input__icon"
+        src="./images/comment.svg"
+        alt="Comment item"
+        />
+        <button class="button" type="submit">Done</button>
+        </form>`);
+        currentHabbit.insertAdjacentHTML('beforeend', form);
+      }
+    page.main.main.append(currentHabbit);
+  });
+}
+
+page.main.main.addEventListener('click', (event) => {
+  if (event.target.matches('.habbit__delete')) {
+    console.log('page children', page.main.main.children)
+    console.log('event ', event.target.closest('.habbit'))
+    // index of habbit.days!!!
+    const index = Array.from(page.main.main.children).indexOf(event.target.closest('.habbit'));
+    console.log('index', index)
+    console.log('active id ', activeID)
+    console.log('habbit of index', habbits[activeID].days[index])
+
+    deleteComment(index);
+  }
+});
+
+function deleteComment(index) {
+  console.log('comment to delete ', habbits[activeID].days[index].comment)
+  habbits[activeID].days[index].comment = '';
+  console.log('new habbits ', habbits)
+
+  saveData(habbits);
+  //console.log('active habbit id', activeHabbit.id)
+  rerender(activeID);
+}
+
+function rerender(activeID) {
+  const activeHabbit = habbits.find(habbit => habbit.id === activeID)
+  if (!activeHabbit) return;
+
+  renderMenu(activeHabbit)
   renderHead(activeHabbit)
+  renderBody(activeHabbit)
 }
 
 
